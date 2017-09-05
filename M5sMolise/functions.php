bca6a7c063b192ac3c00abaed4e7b78e6115fedf
FabriPalladino <FabriPalladino@iMac-de-Marc.local>
2017-09-05 17:35:09 +0200

    First commit

---
+++ M5sMolise/functions.php
@@ -0,0 +1,11 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
