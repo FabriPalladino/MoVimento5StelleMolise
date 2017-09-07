c6db43b50ab920f23ec125dd24527d3eef77fc7a
Fabrizio Palladino <palladino.fabrizio@gmail.com>
2017-09-07 11:01:26 +0200

    Succesfull API call

--- M5sMolise/functions.php
+++ M5sMolise/functions.php
@@ -1,4 +1,14 @@
 <?php
+
+// include custom jQuery
+function m5s_include_custom_jquery() {
+
+	wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+
+}
+add_action('wp_enqueue_scripts', 'm5s_include_custom_jquery');
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
--- M5sMolise/js/main.js
+++ M5sMolise/js/main.js
@@ -1,1 +1,5 @@
-console.log('ciao')
+$(document).ready(function() {
+    $.get('/index.php/wp-json/wp/v2/posts', function(data) {
+        console.log(data);
+    });
+});
