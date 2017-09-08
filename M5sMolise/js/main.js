bbd13cdb0e8116a133df5599bf9a1688b5c3a25d
FabriPalladino <FabriPalladino@iMac-de-Marc.local>
2017-09-08 09:32:34 +0200

    Starting the grid

--- M5sMolise/js/main.js
+++ M5sMolise/js/main.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
-    $.get('/index.php/wp-json/wp/v2/posts', function(data) {
+
+    var catgoriesContainer = $('.categories-labels');
+
+    $.get('/index.php/wp-json/wp/v2/categories', function(data) {
         console.log(data);
+
+        $.each(data, function(index, value){
+
+          var $name = JSON.stringify(value.name);
+
+          $(catgoriesContainer).append('<li>'+
+            $name +
+            '</li>');
+        });
+
     });
+
+    // var newsGrid = $('.news-grid');
+    //
+    // $.get('/index.php/wp-json/wp/v2/posts?filter[category_name]= acqua', function(data) {
+    //     console.log(data);
+    //
+    //     $.each(data, function(index, value){
+    //
+    //       var $parent = JSON.stringify(value.id);
+    //
+    //       //$(newsGrid).find('.main').append(JSON.stringify(value.type));
+    //     });
+    //
+    // });
+
 });
---
+++ M5sMolise/template-parts/grid.php
@@ -0,0 +1,19 @@
+<section class="news-grid">
+  <article class="main">
+
+  </article>
+  <div>
+    <article>
+
+    </article>
+    <article>
+
+    </article>
+    <article>
+
+    </article>
+    <article>
+
+    </article>
+  </div>
+</section>
