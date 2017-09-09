e772c30a5284def5f77a57727251e806e710b5c1
FabriPalladino <FabriPalladino@iMac-de-Marc.local>
2017-09-09 10:28:47 +0200

    update

--- M5sMolise/js/main.js
+++ M5sMolise/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var catgoriesContainer = $('.categories-labels');
 
     $.get('/index.php/wp-json/wp/v2/categories', function(data) {
-        console.log(data);
+        //console.log(data);
 
         $.each(data, function(index, value){
 
@@ -22,18 +22,52 @@ $(document).ready(function() {
 
     });
 
-    var newsGrid = $('.news-grid');
+    var $newsGrid = $('.news-grid');
+
+    var $newsGridMain = $('.news-grid .main');
+    var $newsGridSide = $('.news-grid  article');
+
+    $.each($newsGridSide, function(index, value){
+
+      $(this).addClass('grid-news-'+index+'');
+
+    });
 
     $.get('/index.php/wp-json/wp/v2/posts', function(data) {
-        console.log(data);
+
+
+        var $gridTitles = [];
+        var $idS = [];
 
         $.each(data, function(index, value){
 
           var $title = value.title.rendered;
+          var $id = value.id;
 
-          $('.news-grid .grid-post__title').text($title);
+          $gridTitles.push($title);
+          $idS.push($id);
+
+          $('.grid-news-'+index+'').append('<h2 class="grid-post__title">'+
+          $title+
+          '</h2>');
+
+
         });
 
+        $($newsGridMain).append('<h2 class="grid-post__title">'+
+        $gridTitles[0]+
+        '</h2>');
+
+        console.log($newsGridSide);
+
+
     });
+
+
+
+      $.get('/index.php/wp-json/wp/v2/media', function(data) {
+
+
+      });
 
 });
