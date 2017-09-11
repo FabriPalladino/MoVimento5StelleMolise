60751117d30fc60a606d8b27e1116fd6807f6285
Fabrizio Palladino <palladino.fabrizio@gmail.com>
2017-09-11 10:42:18 +0200

    Dev ri-aligned

--- M5sMolise/js/main.js
+++ M5sMolise/js/main.js
@@ -3,31 +3,71 @@ $(document).ready(function() {
     var catgoriesContainer = $('.categories-labels');
 
     $.get('/index.php/wp-json/wp/v2/categories', function(data) {
-        console.log(data);
+        //console.log(data);
 
         $.each(data, function(index, value){
 
-          var $name = JSON.stringify(value.name);
+          var $name = value.name.split(" ");
+          var $slug = value.slug;
+          var $href = value.link;
 
-          $(catgoriesContainer).append('<li>'+
-            $name +
-            '</li>');
+          $(catgoriesContainer).append('<li class="'+
+            $slug+
+            '">'+
+            '<a href="'+
+            $href+'"><span>'+
+            $name[0] +
+            '</span></a></li>');
         });
 
     });
 
-    // var newsGrid = $('.news-grid');
+    // var $newsGrid = $('.news-grid');
     //
-    // $.get('/index.php/wp-json/wp/v2/posts?filter[category_name]= acqua', function(data) {
-    //     console.log(data);
+    // var $newsGridMain = $('.news-grid .main');
+    // var $newsGridSide = $('.news-grid  article');
+    //
+    // $.each($newsGridSide, function(index, value){
+    //
+    //   $(this).addClass('grid-news-'+index+'');
+    //
+    // });
+    //
+    // $.get('/index.php/wp-json/wp/v2/posts', function(data) {
+    //
+    //
+    //     var $gridTitles = [];
+    //     var $idS = [];
     //
     //     $.each(data, function(index, value){
     //
-    //       var $parent = JSON.stringify(value.id);
+    //       var $title = value.title.rendered;
+    //       var $id = value.id;
     //
-    //       //$(newsGrid).find('.main').append(JSON.stringify(value.type));
+    //       $gridTitles.push($title);
+    //       $idS.push($id);
+    //
+    //       $('.grid-news-'+index+'').append('<h2 class="grid-post__title">'+
+    //       $title+
+    //       '</h2>');
+    //
+    //
     //     });
     //
+    //     $($newsGridMain).append('<h2 class="grid-post__title">'+
+    //     $gridTitles[0]+
+    //     '</h2>');
+    //
+    //     console.log($newsGridSide);
+    //
+    //
     // });
+    //
+    //
+    //
+    //   $.get('/index.php/wp-json/wp/v2/media', function(data) {
+    //
+    //
+    //   });
 
 });
