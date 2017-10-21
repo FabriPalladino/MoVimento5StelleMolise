82e089b2a6e6fa1c7c0a005dfbbec6ebb594479f
FabriPalladino <palladino.fabrizio@gmail.com>
2017-10-21 15:09:27 +0200

    [hotfix] corretto header in full list

--- Molise5Stelle/page-full-list.php
+++ Molise5Stelle/page-full-list.php
@@ -7,7 +7,7 @@
  * @since movimento-5-stelle-molise 1.0
  */
 
-get_header(); ?>
+get_header('home'); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
