ab561f0fb80fa6b365bb9cd1ab1c78ac5f74d9a4
FabriPalladino <palladino.fabrizio@gmail.com>
2017-10-21 15:23:12 +0200

    [hotfix] corretto header in full list

--- Molise5Stelle/template-parts/grid.php
+++ Molise5Stelle/template-parts/grid.php
@@ -4,6 +4,7 @@
   echo 'style="background-image: url(' . $thumb_url[0]  . ')"';
 ?> >
     <span class="grid-post__category"> <a href="?cat=<?php $category = get_the_category(); echo $category[0]->cat_ID; ?>" class="cat-<?php $category = get_the_category(); echo $category[0]->slug; ?>"><?php $category = get_the_category(); echo $category[0]->cat_name; ?> </a></span>
-    <?php the_title( sprintf( '<h2 class="grid-post__title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
+    <a href="<?php echo esc_url( get_permalink() ); ?>" class="grid-link">
+      <?php the_title( sprintf( '<h2 class="grid-post__title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+    </a>
 </article>
