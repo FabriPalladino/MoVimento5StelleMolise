e07883274ee7566bfe2b43d2c88cad6ef363b46a
FabriPalladino <palladino.fabrizio@gmail.com>
2017-10-22 17:08:59 +0200

    [hotfix] Aggiunta hamburgr icon menu mobile

--- Molise5Stelle/header-home.php
+++ Molise5Stelle/header-home.php
@@ -44,7 +44,7 @@
 					<?php endif; ?>
 				</div><!-- .site-branding -->
 				<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
-					<button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'twentysixteen' ); ?></button>
+					<button id="menu-toggle" class="menu-toggle"><?php _e( '<i class="fa fa-bars" aria-hidden="true"></i>', 'twentysixteen' ); ?></button>
 
 					<div id="site-header-menu" class="site-header-menu">
 						<?php if ( has_nav_menu( 'primary' ) ) : ?>
