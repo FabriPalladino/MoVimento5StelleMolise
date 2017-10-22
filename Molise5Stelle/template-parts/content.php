8bc6676fd7e383b8d983494340e77ee2af936e9a
FabriPalladino <palladino.fabrizio@gmail.com>
2017-10-23 00:01:18 +0200

    [hotfix] Aggiunta data alla lista articoli

--- Molise5Stelle/template-parts/content.php
+++ Molise5Stelle/template-parts/content.php
@@ -10,6 +10,15 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+
+		<aside class="article__date">
+			<?php
+				$data = get_the_date();;
+				list($day, $month, $year) = explode(" ", $data);
+				echo '<span>' . $day . ' ' . $month . ' ' . $year . '</span>';
+
+			?>
+		</aside>
 	<?php twentysixteen_m5s_cat(); ?>
 		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
 			<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
@@ -20,7 +29,6 @@
 
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 		<?php twentysixteen_m5s_tag(); ?>
-		<?php //twentysixteen_m5s_author(); ?>
 
 	</header><!-- .entry-header -->
 
